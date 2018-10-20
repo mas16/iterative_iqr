b3bf499457d28e0ce4bdc16ef67e11ad587cbf37
mas16 <mattstetz@gmail.com>
2018-10-20 16:48:53 -0400

    style clean up / markdown update

--- iterative_IQR_v3.py
+++ iterative_IQR_v3.py
@@ -55,10 +55,10 @@ plt.rcParams['axes.linewidth'] = 2.0
 #User input below:
 
 #Enter datapath to file (include .txt extension)
-DATAPATH = 'C:/Users/yao/Desktop/Matt/IQR/ellendata.txt'
+DATAPATH = 'C:/Users/matt/Desktop/github/iterative_iqr/ellendata.txt'
 
 #Enter datapath to output folder
-OUTPATH = 'C:/Users/yao/Desktop/Matt/IQR/'
+OUTPATH = 'C:/Users/matt/Desktop/github/iterative_iqr/'
 
 #Perform iteratively (Y or N)?
 IT_FLAG = 'y'
@@ -68,7 +68,7 @@ IT_FLAG = 'y'
 
 #Open .txt file, read it, and store data
 def read_file(datafile):
-    data = file(datafile,"r")
+    data = file(datafile, "r")
     contents = data.readlines()
     data.close()
     return contents
@@ -86,8 +86,8 @@ def parse_data(datafile):
     return resname, var_1, var_2
 
 #Linear regression model of data
-def linfit(xval,yval):
-    z = np.polyfit(xval,yval,1)
+def linfit(xval, yval):
+    z = np.polyfit(xval, yval, 1)
     return z
 
 #Linear regression model expression 
@@ -97,14 +97,14 @@ def linfit_eq(fit):
     return eq
 
 #Generate predicted y-values from regression model
-def pred_yval(xval,eq):
+def pred_yval(xval, eq):
     pred_y = [eq(entry) for entry in xval]
     return pred_y
 
 #Calculate difference between observed and predicted values
-def gen_difflist(yval,predictedyval):
+def gen_difflist(yval, predictedyval):
     if len(yval) == len(predictedyval):
-        diff = [predictedyval[index]-yval[index] for index in range(len(yval))]
+        diff = [predictedyval[x]-yval[x] for x in range(len(yval))]
         return diff
     else:
         print ("Error: data size mismatch")
@@ -164,31 +164,33 @@ def find_outliers(diffyval):
     return outlier_list        
 
 #Get outlier list index
-def outlier_index(diffyval,outliers):
-    outlierindex_list = [np.where(diffyval == entry)[0][0] for entry in outliers]
-    return outlierindex_list
+def outlier_index(diffyval, outliers):
+    outindex_list = [np.where(diffyval == entry)[0][0] for entry in outliers]
+    return outindex_list
 
 #Get outlier ID using index
-def outlier_id(outlier_index_list,res_list):
+def outlier_id(outlier_index_list, res_list):
     outlier_res_list = [res_list[entry] for entry in outlier_index_list]    
     return outlier_res_list
 
 #Perform statistical analysis
-def run(res,xval,yval):
-    fit = linfit(xval,yval)
-    print ("\nr^2: " + str(np.round((sci.pearsonr(xval,yval)[0])**2,3)))
+def run(res, xval, yval):
+    fit = linfit(xval, yval)
+    print ("\nr^2: " + str(np.round((sci.pearsonr(xval, yval)[0])**2, 3)))
     fiteq = linfit_eq(fit)
-    predictedy = pred_yval(xval,fiteq)
-    diffylist = gen_difflist(yval,predictedy)
+    predictedy = pred_yval(xval, fiteq)
+    diffylist = gen_difflist(yval, predictedy)
     outliers = find_outliers(diffylist)
-    outlierindex = outlier_index(diffylist,outliers)
-    outlierids = outlier_id(outlierindex,res)
+    outlierindex = outlier_index(diffylist, outliers)
+    outlierids = outlier_id(outlierindex, res)
     return outlierids, fiteq
 
 #Remove outliers
-def remove_outliers(res,xval,yval,out):
+def remove_outliers(res, xval, yval, out):
     index_list = [res.index(entry) for entry in out]
-    f_res, f_xval, f_yval = [], [], []
+    f_res = []
+    f_xval = []
+    f_yval = []
     for index in range(len(res)):
         if index not in index_list:
             f_res.append(res[index])
@@ -197,81 +199,88 @@ def remove_outliers(res,xval,yval,out):
     return f_res, f_xval, f_yval
 
 #Remove duplicates
-def remove_duplicate(list1,list2):
+def remove_duplicate(list1, list2):
     if len(list1) > len(list2):
         list1_trim = [entry for entry in list1 if entry not in list2]
-        return list1_trim,list2
+        return list1_trim, list2
     else:
         list2_trim = [entry for entry in list2 if entry not in list1]
         return list1, list2_trim
 
 #Perform outlier identification
-def get_outliers(res,xval,yval):
-    out1,eq = run(res,xval,yval)
+def get_outliers(res, xval, yval):
+    out1,eq = run(res, xval, yval)
     #swap axes
-    out2,eq_s = run(res,yval,xval)
-    out1,out2 = remove_duplicate(out1,out2)
+    out2,eq_s = run(res, yval, xval)
+    out1,out2 = remove_duplicate(out1, out2)
     out = out1 + out2
     return out, eq, eq_s
 
 #Simple plotting function, show outliers in red
-def plot(res,xval,yval,out,eq,eq_s,opath,ext):
-    plt.subplot(2,1,1)
-    plt.plot(xval,yval,'ok')
+def plot(res, xval, yval, out, eq, eq_s, opath, ext):
+
+    plt.subplot(2, 1, 1)
+    plt.plot(xval, yval, 'ok')
+
     for x in range(len(out)):
-        plt.plot(xval[res.index(out[x])],yval[res.index(out[x])],'or')
-    plt.plot(xval,eq(xval),'-k',linewidth = 1.0)
-    plt.title('Fit eq: y ='+str(eq),fontsize = 16)
-    plt.xlabel('Column 1 values',fontsize = 16)
-    plt.ylabel('Column 2 values',fontsize = 16)
-    plt.tick_params(direction = 'out',top = "off",right = "off",width = 2.0,axis = 'both',which = 'major',labelsize = 16)
-    plt.subplot(2,1,2)
-    plt.plot(yval,xval,'ok')
+        plt.plot(xval[res.index(out[x])], yval[res.index(out[x])], 'or')
+
+    plt.plot(xval, eq(xval), '-k', linewidth=1.0)
+    plt.title('Fit eq: y ='+str(eq), fontsize=16)
+    plt.xlabel('Column 1 values', fontsize=16)
+    plt.ylabel('Column 2 values', fontsize=16)
+    plt.tick_params(direction='out', top="off", right="off", width=2.0,
+                    axis='both', which ='major', labelsize=16)
+    plt.subplot(2, 1, 2)
+    plt.plot(yval, xval, 'ok')
+
     for x in range(len(out)):
-        plt.plot(yval[res.index(out[x])],xval[res.index(out[x])],'or')
-    plt.plot(yval,eq_s(yval),'-k',linewidth = 1.0)
-    plt.title('Fit eq: y ='+str(eq_s),fontsize = 16)
-    plt.xlabel('Column 2 values',fontsize = 16)
-    plt.ylabel('Column 1 values',fontsize = 16)
-    plt.tick_params(direction = 'out',top = "off",right = "off",width = 2.0,axis = 'both',which = 'major',labelsize = 16)
-    plt.subplots_adjust(hspace = 0.75)
+        plt.plot(yval[res.index(out[x])],xval[res.index(out[x])], 'or')
+
+    plt.plot(yval,eq_s(yval),'-k',linewidth=1.0)
+    plt.title('Fit eq: y ='+str(eq_s),fontsize=16)
+    plt.xlabel('Column 2 values',fontsize=16)
+    plt.ylabel('Column 1 values',fontsize=16)
+    plt.tick_params(direction='out',top="off",right="off",
+                    width=2.0,axis='both',which='major',labelsize=16)
+    plt.subplots_adjust(hspace=0.75)
     plt.tight_layout()
     plt.savefig(opath+ext+'.png')
     plt.clf()
     return 0
 
 #Start Iterations
-def iterate(res,xval,yval,opath):
-    out,fiteq,fiteq_s = get_outliers(res,xval,yval)
-    plot(res,xval,yval,out,fiteq,fiteq_s,opath,'round1')
+def iterate(res, xval, yval, opath):
+    out, fiteq, fiteq_s = get_outliers(res, xval, yval)
+    plot(res, xval, yval, out, fiteq, fiteq_s, opath, 'round1')
     c = 0
     print ('\noutliers in round ' + str(c+1) + ': ')
     print (out)
     
-    #i prefer to use a while loop
-    #here because a condition must be met to stop iteration
+    #I prefer to use a while loop
+    #here because an unpredictable condition must be met to stop iteration
     while len(out) != 0:
         c += 1
-        res,xval,yval = remove_outliers(res,xval,yval,out)
-        out,fiteq,fiteq_s = get_outliers(res,xval,yval)
-        plot(res,xval,yval,out,fiteq,fiteq_s,opath,'round'+str(c+1))
+        res, xval, yval = remove_outliers(res, xval, yval, out)
+        out, fiteq, fiteq_s = get_outliers(res, xval, yval)
+        plot(res, xval, yval, out, fiteq, fiteq_s, opath, 'round'+str(c+1))
         print ('\noutliers in round ' + str(c+1) + ': ')
         print (out)
-    return res,xval,yval
+    return res, xval, yval
 
-def main(datapath,outpath,it_flag):
-    res,xval,yval = parse_data(split_data(read_file(datapath)))
+def main(datapath, outpath, it_flag):
+    res, xval, yval = parse_data(split_data(read_file(datapath)))
     if it_flag in ['N','No','n','NO']:
-        outliers,fiteq,fiteq_s = get_outliers(res,xval,yval)
+        outliers, fiteq, fiteq_s = get_outliers(res, xval, yval)
         print ('outliers: ')
         print (outliers)
-        plot(res,xval,yval,outliers,fiteq,fiteq_s,outpath,'conventional')
+        plot(res, xval, yval, outliers, fiteq, fiteq_s, outpath, 'conventional')
     elif it_flag in ['Y','Yes','y','YES']:
-        res,xvalL,yval = iterate(res,xval,yval,outpath)
+        res, xval, yval = iterate(res, xval, yval, outpath)
     else:
-        print 'Please select a valid flag for performing iterative IQR: Y or N.'
+        print 'Please select a valid flag for iterative IQR: Y or N.'
         sys.exit()
     return 0
     
 if __name__ == '__main__':
-    main(DATAPATH,OUTPATH,IT_FLAG)
+    main(DATAPATH, OUTPATH, IT_FLAG)
